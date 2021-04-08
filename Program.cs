bc3d7b02bdca5338725bce1d3d1b5593a8ab1ecf
elian169 <78943835+elian169@users.noreply.github.com>
2021-04-08 11:37:31 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f,x,i;
+            int suma, promedio;
+            suma = 0;
+            
+            x = 1;
+            
+            int[] n = new int [4];
+            for ( f=0; f<4; f++)
+            {
+                n[f] = int.Parse(Console.ReadLine());
+                suma = suma + f;
+
+            }
+            
+            promedio = suma / f;
+            Console.WriteLine("datos tecleados");
+            
+
+            for (i = 0; i < 4; i++)
+            {
+                
+                Console.WriteLine(n[i]);
+                
+            }
+
+            
+            promedio = suma /f;
+
+            Console.WriteLine("promedio");
+            Console.Write(promedio);
+        }
+    }
+}
